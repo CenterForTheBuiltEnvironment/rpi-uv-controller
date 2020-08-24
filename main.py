4e0b6cdc7a0ebf57a8d10fe142ce82ae4efd71e7
FedericoTartarini <federicotartarini@gmail.com>
2020-08-24 12:41:05 +0800

    cleaned code and writing data to database

--- main.py
+++ main.py
@@ -1,29 +1,73 @@
 # nvim configuration http://vim.fisadev.com/#features-and-help
 
 from bluepy.btle import Scanner, DefaultDelegate
-import time
 import sqlite3
 import datetime
-import sys
 
-# Connection to the local database
-conn = sqlite3.connect("database.db")
 
-# Create a cursor
-c = conn.cursor()
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except:
+        print("could not connect")
 
-try:
-    c.execute("""CREATE TABLE beacons (
-            time_stamp text,
-            device_id text,
-            rssi integer,
-            )
-            """)
+    return conn
+
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except:
+        print("could not create a table")
+
+
+def add_entry(conn, data):
+    """
+    Create a new entry into the beacons table
+    :param conn:
+    :param data:
+    :return: project id
+    """
+    sql = """ INSERT INTO beacons(device_id, time_stamp, rssi)
+              VALUES(?,?,?) """
+    cur = conn.cursor()
+    cur.execute(sql, data)
     conn.commit()
+    return cur.lastrowid
 
-    print("table successfully created")
-except:
-    print(sys.exc_info()[0])
+
+sql_create_beacons_table = (
+    "CREATE TABLE IF NOT EXISTS beacons "
+    "( id integer PRIMARY KEY, "
+    "device_id text NOT NULL, "
+    "time_stamp text, "
+    "rssi int)"
+)
+
+connection = create_connection("database.db")
+
+# create tables
+if connection is not None:
+    # create projects table
+    create_table(connection, sql_create_beacons_table)
+
+    # create tasks table
+    create_table(connection, sql_create_beacons_table)
+else:
+    print("Error! cannot create the database connection.")
 
 # conn.close()
 
@@ -31,26 +75,23 @@ except:
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
         DefaultDelegate.__init__(self)
-    def handleDiscovery(Self, dev, isNewDev, isNewData):
-        pass
-        # if isNewDev:
-            # pass
-            # print ("Discovered Device", dev.addr)
-        # elif isNewData:
-            # pass
-            # print ("Received new data from:", dev.addr)
+
+    # def handleDiscovery(Self, dev, isNewDev, isNewData):
+    #     if isNewDev:
+    #         print ("Discovered Device", dev.addr)
+    #     elif isNewData:
+    #         print ("Received new data from:", dev.addr)
 
 
 # initialize the scanner
 scanner = Scanner().withDelegate(ScanDelegate())
 
-
 # Main Loop that scans for the beacons
 i = 0
 
 while i < 1:
-#   i = i + 1
-#   print(time.time())
+    #   i = i + 1
+    #   print(time.time())
     print("Scanning ... ")
     devices = scanner.scan(5.0)
     # print("Finished scan for devices")
@@ -59,23 +100,22 @@ while i < 1:
 
         # for (adtype, desc, value) in dev.getScanData():
 
-            # print(adtype, desc, value)
-            # print(dev.addr)
+        # print(adtype, desc, value)
+        # print(dev.addr)
 
-        if dev.addr == ('DA:f7:89:c4:54:5f').lower():
+        if dev.addr == ("DA:f7:89:c4:54:5f").lower():
 
             print("beacon in range")
-            print(dev.addr, dev.addrType, dev.rssi)
+            print(dev.addr, dev.rssi)
 
-            now = datetime.datetime.now().isoformat()
+            reading = (dev.addr, datetime.datetime.now().isoformat(), dev.rssi)
 
-            c.execute(f"INSERT INTO beacons VALUES ( '2020-08-24T11:12:31', 'aa:bb', 20)")
+            index = add_entry(connection, reading)
 
-            c.commit()
+            print(f"index new entry: {index}")
 
-            #if ((desc == 'Complete Local Name') & (value == "ESP32")):
-                # print(desc, value)
-                # ID=str(dev.addr)
-                # print('ID (MAC addr): %s' % (ID))
-                # MQTTID=ID.replace(":","")
-
+            # if ((desc == 'Complete Local Name') & (value == "ESP32")):
+            # print(desc, value)
+            # ID=str(dev.addr)
+            # print('ID (MAC addr): %s' % (ID))
+            # MQTTID=ID.replace(":","")
