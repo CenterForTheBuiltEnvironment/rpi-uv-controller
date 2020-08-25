947e968e9d6b05afa1e20dee08b74dba7646fa7e
FedericoTartarini <federicotartarini@gmail.com>
2020-08-25 16:57:02 +0800

    ultrasonic sensor

--- ultrasonic_sensor.py
+++ ultrasonic_sensor.py
@@ -1,38 +1,6 @@
-#!/usr/bin/python
-import RPi.GPIO as GPIO
-import time
+from gpiozero import DistanceSensor
 
-try:
-      GPIO.setmode(GPIO.BCM)
+ultrasonic = DistanceSensor(echo=17, trigger=4)
 
-      PIN_TRIGGER = 4
-      PIN_ECHO = 17
-
-      GPIO.setup(PIN_TRIGGER, GPIO.OUT)
-      GPIO.setup(PIN_ECHO, GPIO.IN)
-
-      GPIO.output(PIN_TRIGGER, GPIO.LOW)
-
-      print ("Waiting for sensor to settle")
-
-      time.sleep(2)
-
-      print ("Calculating distance")
-
-      GPIO.output(PIN_TRIGGER, GPIO.HIGH)
-
-      time.sleep(0.00001)
-
-      GPIO.output(PIN_TRIGGER, GPIO.LOW)
-
-      while GPIO.input(PIN_ECHO)==0:
-            pulse_start_time = time.time()
-      while GPIO.input(PIN_ECHO)==1:
-            pulse_end_time = time.time()
-
-      pulse_duration = pulse_end_time - pulse_start_time
-      distance = round(pulse_duration * 17150, 2)
-      print ("Distance:",distance,"cm")
-
-finally:
-      GPIO.cleanup()
+while True:
+    print(ultrasonic.distance)
