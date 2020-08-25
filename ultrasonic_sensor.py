539aa82d06b9a4d5c69b5e9775f6d9f8781e7faf
FedericoTartarini <federicotartarini@gmail.com>
2020-08-25 17:00:03 +0800

    ultrasonic sensor

--- ultrasonic_sensor.py
+++ ultrasonic_sensor.py
@@ -1,6 +1,9 @@
 from gpiozero import DistanceSensor
+import time
 
-ultrasonic = DistanceSensor(echo=17, trigger=4)
+ultrasonic = DistanceSensor(echo=17, trigger=4, max_distance=2)
 
 while True:
     print(ultrasonic.distance)
+
+    time.sleep(1)
