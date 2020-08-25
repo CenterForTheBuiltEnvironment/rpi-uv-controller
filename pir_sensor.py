53cd38d834c08b0431e58f34281e8fb387c3a887
FedericoTartarini <federicotartarini@gmail.com>
2020-08-25 10:48:27 +0800

    improving pir code

--- pir_sensor.py
+++ pir_sensor.py
@@ -7,10 +7,18 @@ GPIO.setup(6, GPIO.IN)  # Read output from PIR motion sensor
 
 while True:
     i = GPIO.input(6)
-    print(i)
-    if i == 0:  # When output from motion sensor is LOW
-        print ("No motion detected", i)
-        time.sleep(1)
-    elif i == 1:  # When output from motion sensor is HIGH
-        print ("motion detected")
-        time.sleep(1)
+
+    motion_array = []
+    for time in range(0, 120):
+
+        if i == 0:  # When output from motion sensor is LOW
+            time.sleep(0.5)
+            motion_array.append(False)
+        elif i == 1:  # When output from motion sensor is HIGH
+            time.sleep(0.5)
+            motion_array.append(True)
+
+    if True in motion_array:
+        print("motion was detected")
+    else:
+        print("no motion detected")
