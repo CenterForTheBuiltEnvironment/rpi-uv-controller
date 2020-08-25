ade7e6c2ba60c7dfde4fd0fdddd10dccab5bc18b
FedericoTartarini <federicotartarini@gmail.com>
2020-08-25 16:20:53 +0800

    deleted capacitive_switch.py

--- stop_code.sh
+++ stop_code.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # kill all the python scripts running
+# https://stackoverflow.com/questions/40652793/how-to-kill-python-script-with-bash-script
+# https://www.cyberciti.biz/faq/how-to-check-running-process-in-linux-using-command-line/
 sudo pkill -f pir_sensor.py
 sudo pkill -f beacon_scanner.py
 sudo pkill -f light_controller.py
