6e7e79a311eb6a847d1f4115a167a224ed09fe2d
FedericoTartarini <federicotartarini@gmail.com>
2020-08-26 10:43:36 +0800

    ultrasonic sensor

--- ultrasonic_sensor.py
+++ ultrasonic_sensor.py
@@ -1,9 +1,24 @@
 from gpiozero import DistanceSensor
 import time
+import numpy as np
+
+distances = []
 
 ultrasonic = DistanceSensor(echo=17, trigger=4, max_distance=2)
 
 while True:
-    print(ultrasonic.distance)
+
+    distance = ultrasonic.distance
+
+    distances.append(distance)
+
+    if len(distances) > 120:
+
+        del distances[0]
+
+        mean = np.mean(distances)
+        std = np.std(distances)
+
+        print(mean, std)
 
     time.sleep(1)
