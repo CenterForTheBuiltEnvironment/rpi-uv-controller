8dbeaf62d2e04ce27b88ee8374c945b67b09ca7c
FedericoTartarini <federicotartarini@gmail.com>
2020-08-28 13:57:36 +0800

    fixing errors

--- kill_switch.py
+++ kill_switch.py
@@ -3,5 +3,7 @@ button = Button(2)
 
 if __name__ == '__main__':
 
-    button.wait_for_press()
-    print('You pushed me')
+    while True:
+
+        button.wait_for_press()
+        print('You pushed me')
