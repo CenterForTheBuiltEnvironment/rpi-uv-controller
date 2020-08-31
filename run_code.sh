47c9477b375b22ffe5bfeaaabaadd426c0ea30d0
FedericoTartarini <federicotartarini@gmail.com>
2020-08-31 11:11:33 +0800

    added back sudo to run_code.sh

--- run_code.sh
+++ run_code.sh
@@ -10,11 +10,11 @@ rm -f database.db
 echo "Starting python scripts"
 # start python scripts
 sudo python3 beacon_scanner.py &
-python3 pir_sensor.py &
-python3 ultrasonic_sensor.py &
-python3 kill_switch.py &
+sudo python3 pir_sensor.py &
+sudo python3 ultrasonic_sensor.py &
+sudo python3 kill_switch.py &
 
 sleep 10
 
 # start controller
-python3 light_controller.py &
+sudo python3 light_controller.py &
